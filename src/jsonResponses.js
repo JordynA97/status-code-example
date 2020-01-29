20cf58a21ffa2d387a821ac1832a170fba393f36
JordynA97 <jla5483@rit.edu>
2020-01-29 14:49:14 -0500

    complete

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -17,6 +17,15 @@ const badRequest = (request, response, params) => {
     message: 'This request has the required parameters',
   };
 
+  if(!params.valid || params.valid !== 'true'){
+    responseJSON.message = 'Missing valid query parameter sent to true';
+    responseJSON.id = 'badRequest';
+
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  return respondJSON(request, response, 200, responseJSON);
+
 };
 
 const notFound = (request, response) => {
@@ -25,6 +34,7 @@ const notFound = (request, response) => {
     id: 'notFound',
   };
 
+  respondJSON(request, response, 404, responseJSON);
 };
 
 module.exports = {
